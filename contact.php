175da30f869e8407faae3722d82756f9a614870e
LauraGh <laurac.gheorghe@gmail.com>
2013-04-13 15:56:58 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -74,18 +74,9 @@ E-mail: <a href="mailto:razvan.deaconescu@cs.pub.ro">razvan.deaconescu@cs.pub.ro
 <dt>EG306</dt>
 <dd>
 Program: <b>L-V: 12:00 - 17:00</b><br/>
-Persoană de contact: Călin Burloiu<br/>
-E-mail: <a href="mailto:calin.burloiu@gmail.com">calin.burloiu@gmail.com</a><br/>
+Persoană de contact: Laura Vasilescu<br/>
+E-mail: <a href="mailto:vasilescu.laura@gmail.com">vasilescu.laura@gmail.com</a><br/>
 </dd>
-
-<dt>ED422</dt>
-<dd>
-Program: <b>Ma: 15:00 - 20:00, J: 12:00 - 20:00</b><br/>
-Persoană de contact: Laura Gheorghe<br/>
-E-mail: <a href="mailto:laura.gheorghe@cs.pub.ro">laura.gheorghe@cs.pub.ro</a><br/>
-</dd>
-
-
 
 </dl>
 				</div><!-- .col-big -->
@@ -99,6 +90,7 @@ Pentru mai multe informații despre această inițiativă, vă rugăm să trimit
 					<br/>
 					<p><b>Laura Gheorghe</b></p>
 					<p><a href="mailto:laura.gheorghe@cs.pub.ro">laura.gheorghe@cs.pub.ro</a></p>
+					<p>Telefon: 0748094267</p>
 					<br/>
 					<p><b>Raluca Podiuc</b></p>
 					<p><a href="mailto:ralucaelena1985@gmail.com">ralucaelena1985@gmail.com</a></p>
@@ -106,7 +98,7 @@ Pentru mai multe informații despre această inițiativă, vă rugăm să trimit
 
 <p><br/><br/></p>
 					<h2>Mulțumiri</h2>
-<p>Mulțumiri echipei adunate anul acesta pentru susținerea inițiativei: Mircea&nbsp;Bardac, Alex&nbsp;Eftimie, Andrei&nbsp;Epure, Liviu&nbsp;Ioan, Răzvan&nbsp;Deaconescu, Călin&nbsp;Burloiu<p>
+<p>Mulțumiri echipei adunate anul acesta pentru susținerea inițiativei: Mircea&nbsp;Bardac, Alex&nbsp;Eftimie, Catalin Dogaru, Andrei&nbsp;Epure, Liviu&nbsp;Ioan, Răzvan&nbsp;Deaconescu, Călin&nbsp;Burloiu, Laura Vasilescu<p>
 				</div><!-- .col -->
 
 				<div class="clear">
